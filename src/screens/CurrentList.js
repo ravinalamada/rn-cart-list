af9de682eed086562b5bd687ffabe82d4c314921
Prisca Roseline <prisca.ros@onja.org>
2021-04-16 11:25:29 +0300

    set loading age

--- src/screens/CurrentList.js
+++ src/screens/CurrentList.js
@@ -1,21 +1,59 @@
-import React, { useState } from 'react';
-import { View, Text, SafeAreaView, FlatList, KeyboardAvoidingView } from 'react-native'
-import nachos from '../data/nachos';
+import React, { useState, useEffect } from 'react';
+import { View,
+         Text,
+         SafeAreaView,
+         FlatList,
+         KeyboardAvoidingView,
+         ActivityIndicator } from 'react-native'
+// import nachos from '../data/nachos';
 import {v4 as uuid } from 'uuid';
+import AsyncStorage from '@react-native-community/async-storage';
 import ListItem, {Separator} from '../components/ListItem';
 import AddItem from '../components/addItem';
 
+const updateStoredCurrentList = (list) => {
+  AsyncStorage.setItem('@@GroceryList/currentList', JSON.stringify(list));
+}
+
 export default () => {
-  const [list, setList] = useState(nachos)
+  const [list, setList] = useState([])
+  const [loading, setLoading] = useState(true);
 
   const addItem = (text) => {
-    setList([{id: uuid(), name: text},...list])
+    const newList = [{id: uuid(), name: text},...list];
+    setList(newList);
+    updateStoredCurrentList(newList)
   }
 
   const removeItem = (id) => {
-    const newList = list.filter(item => item.id === id);
+    const newList = list.filter(item => item.id !== id);
     setList(newList)
+    updateStoredCurrentList(newList)
   }
+
+  useEffect(() => {
+    setTimeout(() => {
+      AsyncStorage.getItem('@@GroceryList/currentList')
+      .then(data => JSON.parse(data))
+      .then(data => {
+        if(data) {
+          setList(data);
+        }
+        setLoading(false)
+      })
+    }, 1000)
+  }, [])
+
+  if(loading) {
+    return (
+      <SafeAreaView>
+        <Text>
+          loading
+        </Text>
+      </SafeAreaView>
+    )
+  }
+
   return (
     <SafeAreaView style={{flex: 1}}>
       <KeyboardAvoidingView
