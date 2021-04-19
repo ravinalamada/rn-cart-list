01c70d93fe2ebdd3e1f084ce546c40ff19194d73
Prisca Roseline <prisca.ros@onja.org>
2021-04-19 09:44:50 +0300

    build navigation

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Text} from 'react-native';
 import CurrentList from '../screens/CurrentList';
 import ItemDetails from '../screens/ItemDetails'
 import {createStackNavigator} from '@react-navigation/stack';
@@ -11,8 +12,16 @@ const CurrentListStack = () => {
   return(
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="CurrentList" component={CurrentList} />
-        <Stack.Screen name="ItemDetails" component={ItemDetails} />
+        <Stack.Screen name="Shopping List" component={CurrentList} />
+        <Stack.Screen name="ItemDetails" component={ItemDetails}
+           options={({route}) => {
+             return {
+               headerTitle: () => {
+                 return <Text>{route.params.item.name}</Text>
+               }
+             }
+           }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   )
--- src/screens/ItemDetails.js
+++ src/screens/ItemDetails.js
@@ -1,1 +1,10 @@
-export default () => null;
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default ({route}) => {
+ return(
+  <View>
+      <Text>{JSON.stringify(route.params.item, null, 2)}</Text>
+    </View>
+ )
+};
